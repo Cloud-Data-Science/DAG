3c0f811b60f1aa0b71a6ec45f144fb14c3924ba4
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2021-10-12 10:30:47 +0200

    Pushed DAG etl-1012103046

---
+++ etl-1012103046.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "etl-1012103046",
+}
+
+dag = DAG(
+    "etl-1012103046",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="Created with Elyra 3.1.1 pipeline editor using `etl.pipeline`.",
+    is_paused_upon_creation=False,
+)
+
+
+op_a8cc213a_6708_48c2_812b_046d1246bc9b = KubernetesPodOperator(
+    name="input",
+    namespace="airflow",
+    image="continuumio/anaconda3:2020.07",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.1.1/elyra/airflow/bootstrapper.py --output bootstrapper.py && curl -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.1.1/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --cos-endpoint http://myminio.server:9000 --cos-bucket store --cos-directory 'etl-1012103046' --cos-dependencies-archive 'input-data-a8cc213a-6708-48c2-812b-046d1246bc9b.tar.gz' --file 'input/input-data.ipynb' "
+    ],
+    task_id="input",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minioadmin",
+        "AWS_SECRET_ACCESS_KEY": "minioadmin",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "etl-1012103046-{{ ts_nodash }}",
+    },
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
